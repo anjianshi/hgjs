0c283233db106e14c89088ef4b7bb63ccb80c61f
anjianshi <anjianshi@gmail.com>
2016-11-11 16:47:27 +0800

    新增 MoneyInput component

---
+++ src/components/MoneyInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// 用来输入金额数据的文本框。会把原始的以“厘”为单位的数值转换为以“元”为单位再显示在文本框内部
+// 搭配 form 下的 MoneyValidator 使用
+// 详见 [处理浮点数](https://github.com/anjianshi/hgjs/blob/master/doc/%E5%A4%84%E7%90%86%E6%B5%AE%E7%82%B9%E6%95%B0.adoc)
+export const MoneyInput = React.createClass({
+    render() {
+        let value = this.props.value
+        if(typeof value === 'number') {
+            // 在文本框中，禁止在金额是整数时补充 .00 后缀，因为这会给用户的输入增添麻烦
+            value = value.toPlainMoney(false)
+        }
+        return <input {...this.props} value={value} />
+    }
+})
