2284653b9ce5aa759f69cca918f9521d0f593bb2
anjianshi <anjianshi@gmail.com>
2017-03-01 21:56:58 +0800

    extraProps() 支持传入自定义的 props

--- src/component/functions.js
+++ src/component/functions.js
@@ -1,7 +1,11 @@
 import { omit, keys } from 'lodash'
 
-// 返回当前 component 接收到的、没有定义在 propTypes 里的 props。
-// 这些 props 通常是用来传给下级 component 的。
-export function extraProps(componentInstance) {
-    return omit(componentInstance.props, keys(componentInstance.constructor.propTypes))
+/*
+返回当前 component 接收到的、没有定义在 propTypes 里的 props。
+这些 props 通常是用来传给下级 component 的。
+
+若未指定 props，则从 component.props 中提取；否则从给出的 props 中提取（例如可以把 componentWillReceiveProps() 里接收到的 nextProps 传给此函数）
+*/
+export function extraProps(componentInstance, props=null) {
+    return omit(props || componentInstance.props, keys(componentInstance.constructor.propTypes))
 }
