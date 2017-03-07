966f0baa29d72e83a0bbe7f6820ce23d2aba354c
anjianshi <anjianshi@gmail.com>
2017-03-07 13:13:37 +0800

    persistCache 工具改为使用 exJSON 系列函数以支持 Map 和 Set

--- src/cache.js
+++ src/cache.js
@@ -1,11 +1,14 @@
 /* eslint-env browser */
 
+import { exToJSON, exParseJSON } from 'lang'
+
 /*
 缓存 app 数据（例如 view component 的 state）以便以后使用（例如在 component unmount 后重新 mount 时，取回之前的 state）。
 数据为单次取出，即在取出后，就会把数据从 cache 中清除。
 
 cache 中的数据存储在内存里，性能更好，且允许存入不可序列化的数据（例如 Promise 对象）
 persistCache 里的数据即使关闭浏览器再打开也依然有效，但里面的数据必须能够 JSON 化。
+（使用了经扩展的 JSON 函数，支持处理 Map 和 Set 类型）
 
 随着 app 代码的更新，之前存入的 persist cache 可能会不符合新代码里所需的格式，并导致不可预知的问题。
 可以通过指定 keyPrefix，并在每次 persist cache 格式变化时更改 prefix，来解决此问题（因为这样一来，之前存入的 cache 就都不会被读取到了）
@@ -42,14 +45,14 @@ export const persistCache = {
 
     set(key, data) {
         key = persistCache.keyPrefix + key
-        data = JSON.stringify(data)
+        data = exToJSON(data)
         localStorage[key] = data
     },
 
     get(key) {
         if(!persistCache.has(key)) return undefined
 
-        const data = JSON.parse(localStorage[key])
+        const data = exParseJSON(localStorage[key])
         delete localStorage[key]
         return data
     }
