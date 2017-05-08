02a08275616a4b687cba1079fcd955823ecbdc63
anjianshi <anjianshi@gmail.com>
2017-05-08 10:40:36 +0800

    重新实现 request，改为基于 Fetch API

--- src/request.js
+++ src/request.js
@@ -1,143 +1,80 @@
-/**
-AJAX Library
-- 参考 qwest，精简了一些功能，也不再支持那么多浏览器，并修正了一些不符合要求的行为，且代码统一以 ES6 的形式进行重写
-- request data type 支持 query string (GET 请求)、JSON 和 form-data (用于上传文件)
-- response Content-Type 为 json 时，自动对返回值进行解析；否则把返回值当作纯文本来处理
-- 支持 timeout 和超时重试
-- 不支持跨域 / JSONP；不支持 sync 请求
-- 浏览器支持到 IE11（主要是 IE9 不支持 FormData；对于 IE10 现在其实并没有看到用到哪些它不支持的功能，所以其实可能也是支持的）
-
-XMLHTTPRequest 相关资料：
-https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
-https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-http://www.w3.org/TR/2012/WD-XMLHttpRequest-20120117/#event-xhr-load
-*/
-
-import { queryString } from 'lang'
+import 'whatwg-fetch'
 import { Promise } from 'promise'
-import { get } from 'lodash'
+import { queryString } from 'lang'
+import { isPlainObject } from 'lodash'
+import invariant from 'invariant'
 
 
 /*
-options:
-    method       默认为 GET
-    data         若 method 为 GET，data 会被拼装成 query string 附加到 url 后面
-    headers      额外的请求头
+发起一个 AJAX 请求（实为对 Fetch API 的简单封装）
+- 将 Fetch 返回的原生 Promise 转换为 Bluebird Promise（它的好处是支持 cancel() 操作）
+- 扩展原 fetch() 函数 init 参数的 body 属性，若传入的是 plain object，会将其转换成 JSON 字符串
+- 最终解析出的 response 对象增加一个 parsedBody 属性。response Content-Type 为 json 时，其为经过解析的 JSON 数据；否则为纯文本。
+  这样就不用在接收到 response 对象后，再进行一次 promise then 操作来获取实际数据了。
 
-    timeout      请求的超时时间(ms)，默认为 30000
-    attempts     若出现请求超时的情况，总共允许执行几次请求（即是否允许重试。1 代表不重试，2 代表重试1次，以此类推）
+= 关于“超时”与“超时重试”
+早期的 makeRequest() 函数是基于 XHR 的。
+那时不用 Fetch API 的一个重要原因，是它没有指定 timeout 和超时重试的功能。
+也就是一个请求如果在若干时长内还没有完成，就判断为请求失败，重新发起请求或抛出异常。
+但其实这样的机制没有必要。
+之前的想法是，一个请求如果长时间没有完成，就说明网络有问题，那么重新连一次就有可能好转；或是不重新请求，直接报错，向用户告知网络连接不上。
+但其实，如果是当前设备没有联网，请求通常一开始就会失败，并不会长时间挂着；长时间挂着要么是网络状况不好（如丢包严重），要么是服务器很忙，回复速度慢。
+此时，重新发起请求一般只会让情况变得更差（一些之前已进行完毕的进程又要重新走一遍），而不会提前完成请求。
+因此，超时重连功能是没用必要的。
+那如果我们不想超时重连，只是想在超时时终止请求呢？
+这我们首先要看，终止完请求后我们接下来想要做什么。如果终止请求后还是引导用户再按按钮发起一次请求，那和超时重连就没有什么区别。
+如果是认为一个请求在超出一定时限后就没有必要执行了，因为这种情况很少，完全可以由发起请求的代码手动进行这一控制，而不必非得要求 AJAX 类库内建这一功能。
+并且，其实我们可以把控制权交给用户，让用户在请求执行过程中有办法取消请求（包括点取消按钮、点返回上一页按钮等），这样如果用户等不及了，他就会自己去取消，如果他愿意等，那就让他一直等下去。
+这样我们甚至都完全不用手动控制请求时长。
 
-    preprocessor     (xhr) => {}         可以通过此回调，在请求正式发送前，对 xhr 对象进行一些处理。
+= 关于 cancel
+Bluebird Promise 支持 cancel 操作，但此操作并不会真的结束 HTTP 请求（Fetch API 也不支持结束请求），
+只会使 .then() 和 .catch() 回调不会被调用（.finally() 回调扔会被调用）。
 
-此函数返回一个 bluebird Promise，
-- 若请求成功，它会以 resolved 的形式结束，返回 response 数据；
-- 若请求出现错误或超时，它会以 rejected 的形式结束，返回 RequestError 错误；
-- 通过 promise.finally(fn) 可以指定一个在 resolved 或 rejected 状态下皆会被调用的回调。它可以用来进行一些收尾工作。
-- 通过 promise.cancel() 方法可以手动终止请求，这种情况下，then() 或 catch() 回调都不会被调用，但 finally() 回调仍会被调用。
-
-注意：无论是因超时而结束请求，还是用户手动结束请求，都不会真的结束 HTTP 连接，只是这个 Promise 将会被结束。
-这是因为结束连接（xhr.abort()）这样的行为是没有保障的。
-我们无法保证在我们执行它的时候，后端是否已经接收到请求并开始处理，也无法保证我们断开连接后，后端会不会继续处理。
+之所以不想办法实现成真多结束请求，是因为这样的行为是没有保障的。
+我们无法保证在我们想要结束请求时，后端是否已经接收到请求并开始处理，也无法保证我们断开连接后，后端会不会继续处理。
 因此，干脆抛弃它。不然，它反而会干扰我们的决策，让我们以为执行了它就能让正在进行的操作被取消。
 与其这样想，不如把我们的应用设计成即使操作没被取消也没问题。例如即使一个操作被重复执行了两次也不会出错。
 */
-export function makeRequest(url, options={}) {
-    const opt = formatOptions(url, options)
+export function makeRequest(url, options=null) {
+    options = {...options}
 
-    let resolve, reject
-    const promise = new Promise((_resolve, _reject) => {
-        resolve = _resolve
-        reject = _reject
+    if(!options.headers) options.headers = {}
+    invariant(isPlainObject(options.headers), 'options.headers 只能是 plain object，不支持 Headers 等对象')
+
+    if(isPlainObject(options.body)) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(options.body)
+    }
+
+    return new Promise((resolve, reject) => {
+        window.fetch(url, options)
+            // HTTP 请求完成，status 有可能是 200，也有可能是 404、500 等
+            .then(response => {
+                const contentType = response.headers.get('Content-Type') || ''
+                const getBody = contentType.startsWith('application/json') ? response.json : response.text
+                return getBody.call(response).then(body => {
+                    response.parsedBody = body
+                    resolve(response)
+                })
+            })
+            // HTTP 请求未完成或在解析 parsedBody 时出错（例如 JSON 不合法）
+            .catch(reject)
     })
-
-    const xhr = new XMLHttpRequest()
-
-    // 只要请求成功结束，即使 status code 不是 200，浏览器也会认为是成功结束；但对我们来说，应该将它视为请求失败
-    xhr.addEventListener('load', () => complete(xhr.status === 200 ? 'success' : 'error'))
-    xhr.addEventListener('error', () => complete('error'))
-
-    // 发送请求
-    // 若指定了 timeout 和 attempts，当请求超时时还会调用此函数重新发起请求。
-    function send() {
-        xhr.open(opt.method, opt.url)
-        for(const [name, value] of Object.entries(opt.headers)) xhr.setRequestHeader(name, value)
-        if(opt.preprocessor) opt.preprocessor(xhr)
-        xhr.send(opt.data)
-    }
-
-    function complete(status) {
-        const response = parseResponse(xhr)
-        if(status === 'success') {
-            resolve(response)
-        } else if(status === 'error') {
-            reject(new RequestError(xhr, response))
-        }
-    }
-
-    if(opt.timeout) {
-        var alreadyAttempts = 1
-        const intervalId = setInterval(() => {
-            if(alreadyAttempts < opt.attempts) {
-                // 尝试重新发起请求
-                alreadyAttempts++
-                send()
-            } else {
-                // 已达到重试次数，不再重新发起请求，以 error 的形式结束。
-                complete('error')
-            }
-        }, opt.timeout)
-        promise.catch(() => {}).finally(() => clearInterval(intervalId))
-    }
-
-    send()
-
-    return promise
 }
 
-function formatOptions(url, raw) {
-    const opt = {
-        url,
-        method: raw.method ? raw.method.toUpperCase() : 'GET',
-        headers: {...raw.headers},
-        data: null,
 
-        timeout: get(raw, 'timeout', 30000),
-        attempts: get(raw, 'attempts', 1),
+/*
+向 url 末尾添加 query string
+url:  url 已经带上 query string 或没有带上均可
+qs:   query string 内容。可以是字符串或对象（会被序列化为字符串）
+*/
+export function appendQS(url, qs) {
+    if(typeof qs !== 'string') qs = queryString.stringify(qs)
 
-        preprocessor: raw.preprocessor
-    }
+    const sep = url.indexOf('?') < 0
+        ? '?'
+        : (url.endsWith('&') ? '' : '&')
 
-    if(raw.data) {
-        if(opt.method === 'GET') {
-            url += (/\?/.test(url) ? '&' : '?') + queryString.stringify(raw.data)
-        } else if((window.FormData && raw.data instanceof window.FormData)
-                || (window.ArrayBuffer && raw.data instanceof ArrayBuffer)) {
-            opt.data = raw.data
-        } else {
-            opt.headers['Content-Type'] = 'application/json'
-            opt.data = JSON.stringify(raw.data)
-        }
-    }
-
-    return opt
+    return url + sep + qs
 }
-
-function parseResponse(xhr) {
-    if(xhr.status === 0) {  // 说明请求是以 abort 的方式结束的
-        return null
-    } else {
-        // 在 IE9 中，xhr 对象没有 response 属性。
-        const rawResp = typeof xhr.response !== 'undefined' ? xhr.response : xhr.responseText
-        return xhr.getResponseHeader('Content-Type').startsWith('application/json')
-            ? JSON.parse(rawResp)
-            : rawResp
-    }
-}
-
-// 自定义 Error 类的方法来自： http://stackoverflow.com/a/871646/2815178
-function RequestError(xhr, response) {
-    this.xhr = xhr
-    this.response = response
-}
-RequestError.prototype = Error.prototype
