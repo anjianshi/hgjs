fa2994bef11a0ce6dbeedc986a4da78a11fecbca
anjianshi <anjianshi@gmail.com>
2017-06-27 23:12:47 +0800

    新增 SharedModal 工具

---
+++ src/native/components/SharedModal.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Modal, StyleSheet, View } from 'react-native'
+import invariant from 'invariant'
+
+
+/*
+hgjs 以及 app 代码中的很多工具／组件都要用到 <Modal>。使用者每次使用它们时，都要在页面里额外挂载一个 <Modal>，比较麻烦。
+通过此工具，可以解决此问题：只要在 app 里挂载一次此工具提供的 modal component，然后在所有需要显示 modal 的地方就都可以共用这个实例。
+并提供函数接口，方便 modal 的开启和关闭。
+
+使用方式：
+在 app 里挂载此 <SharedModal/> component，然后所有依赖此 component 的工具／组件就都可以工作了。
+*/
+
+
+let instance = null
+
+
+export function openModal(...args) {
+    invariant(instance, 'SharedModal: instance 不存在，请检查 <SharedModal> 是否正常挂载')
+    instance.open(...args)
+}
+
+export function closeModal() {
+    invariant(instance, 'SharedModal: instance 不存在，请检查 <SharedModal> 是否正常挂载')
+    instance.close()
+}
+
+
+export class SharedModal extends React.Component {
+    // props 均会传给 <Modal>
+
+    state = {
+        ContentComponent: null,
+        contentProps: null
+    }
+
+    componentWillMount() {
+        invariant(!instance, 'SharedModal: instance 已存在，请检查 <SharedModal> 是否重复挂载')
+        instance = this
+    }
+
+    componentWillUnmount() {
+        instance = null
+    }
+
+    open = (ContentComponent, contentProps, modalProps) => {
+        invariant(!this.state.ContentComponent, 'SharedModal: modal 已经处于开启状态，不支持同时开启多个 modal')
+
+        this.setState({
+            ContentComponent,
+            contentProps,
+            modalProps,
+        })
+    }
+
+    close = () => {
+        this.setState({
+            ContentComponent: null,
+            contentProps: null,
+            modalProps: null,
+        })
+    }
+
+    render() {
+        const { ContentComponent, contentProps, modalProps } = this.state
+
+        // 不显示 modal 时，指定一个 onRequestClose 回调的占位符。不然 Android 下会出现警告
+        // 显示 modal 时，仍要求使用者自行进行指定
+        const preparedProps = !ContentComponent && { onRequestClose: () => {} }
+
+        return <Modal {...preparedProps} transparent animationType="fade" {...this.props} {...modalProps} visible={!!ContentComponent}>
+            <View style={s.bg}>
+                <If condition={ContentComponent}>
+                    <ContentComponent {...contentProps} />
+                </If>
+            </View>
+        </Modal>
+    }
+}
+
+
+
+const modalOverlay = 'rgba(20,20,20,0.5)'
+
+const s = StyleSheet.create({
+    bg: {
+        backgroundColor: modalOverlay,
+        flex: 1,
+    }
+})
--- src/native/components/index.js
+++ src/native/components/index.js
@@ -1,3 +1,4 @@
 export { AutoSizeImage } from './AutoSizeImage'
 export { Central } from './Central'
 export * from './form'
+export * from './SharedModal'
